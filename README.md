e15fbf2f9ee553a3bb249e7d8f2f489e33110e53
Pedro Stanzani <pedrohstanzani@gmail.com>
2025-01-07 10:35:26 -0300

    Add Vercel Analytics

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
+## Roadmap
+- Missing import for button (submit button)
+- Change component name from ProfileForm
+
 ## Getting Started
 
 First, run the development server:
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/next';
 
 import "./globals.css";
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
